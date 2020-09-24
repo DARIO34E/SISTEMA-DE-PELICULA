3957bd7f2bbb26312a1e57fe842b893cee756ca2
DARIO34E <64812170+DARIO34E@users.noreply.github.com>
2020-09-24 05:54:21 -0500

    Add files via upload

---
+++ controllers/RentadasController.php
@@ -0,0 +1,111 @@
+<<<?php
+require 'models/Rentadas.php';
+require 'models/Status.php';
+require 'models/User.php';
+require 'models/Category.php';
+/**
+ * 
+ */
+class RentadasController
+{
+    private $model;
+    private $users;
+    private $status;
+    private $category;
+
+    public function __construct()
+    {
+        try{
+            $this->model = new Rentadas;
+            $this->users = new User;
+            $this->status = new Status;
+            $this->category = new Category;
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+    
+    public function index(){
+        require 'views/layout.php';
+        $renta=$this->model->getAll();
+        require 'views/Rentadas/list.php';
+    }
+
+    public function new()
+    {
+        $users = $this->users->getAll();
+        $categories = $this->category->getAll();
+        require 'views/layout.php';
+        require 'views/rentadas/new.php';
+    }
+    public function save()
+    {
+        $this->model->newRentadas($_REQUEST);
+        header('Location: ?controller=Rentadas');
+    }
+    
+    public function edit()
+	{
+		if (isset($_REQUEST['id'])) {
+			$id = $_REQUEST['id'];
+			$data = $this->user->getById($id);
+			$statuses = $this->status->getCustomStatuses('usuario');
+			$roles = $this->role->getAll();
+			require 'views/layout.php';
+			require 'views/Renta/edit.php';
+		} else {
+			echo "Error, no se realizo";
+		}
+	}
+    public function saved()
+    {
+        //Organizar en un array los datos de la tabla movie
+        $dataMovie = [
+            'name'          => $_POST['name'],
+            'description'   => $_POST['description'],
+            'user_id'       => $_POST['user_id'],
+            'status_id'     => 1
+        ];
+
+        //Array de categorias
+        $arrayCategories = isset($_POST['categories']) ? $_POST['categories'] : [];        
+
+        if(!empty($arrayCategories)) {
+            //Inserción de la Tabla Movie
+            $respMovie = $this->model->newRentadas($dataMovie);
+
+            //Obtener el ultimo ID registrado
+            $lastIdMovie = $this->model->getLastId();
+            //Inserción de la Tabla category_movie
+            $respCategoryMovie = $this->model->saveCategoryMovie($arrayCategories, $lastIdMovie[0]->id);
+
+        } else {
+            $respMovie          = false;
+            $respCategoryMovie  = false;            
+        }
+
+        $arrayResp = [];
+
+        if($respMovie == true && $respCategoryMovie == true) {
+            $arrayResp = [
+                'success' => true,
+                'message' => "Pelicula Creada"  
+            ];
+        } else {
+            $arrayResp = [
+                'error' => true,
+                'message' => "Error Creando la Pelicula"  
+            ];
+        }
+
+        echo json_encode($arrayResp);
+        return;
+
+    }
+    
+   
+    
+
+}
+
+?>
---
+++ controllers/RoleController.php
@@ -0,0 +1,20 @@
+<?php
+require 'models/Role.php';
+require 'models/Status.php';
+
+ 
+class RoleController{
+    private $model;
+    private $status;
+
+   
+    public function index()
+    {
+        /*$roles = $this->model->getAll();
+        var_dump($roles);
+        require 'views/layout.php';
+        require 'views/roles/list.php';*/
+    }
+
+     
+ }
